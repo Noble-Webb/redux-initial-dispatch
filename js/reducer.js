e5740663b9caeab23e62865d20392b784ad10943
Justin_Webb <Webb.n.justin@gmail.com>
2020-11-30 13:50:47 -0600

    Practice setting initial cation and state values

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,6 @@
-let state = {count: 0};
-
-function changeState(state, action){
+let state;
+//sets the initial value of state in change state
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -17,3 +17,13 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+// render()
+
+// shows the initial state by sending inane type triggering default
+// works as it calls the changeState function which
+// sets the initial state count to 0
+dispatch({ type: '@@INIT' })
+
+// calls the function to trigger increase case 
+// dispatch({type: "INCREASE_COUNT"})
